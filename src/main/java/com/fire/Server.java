a4f5f652a70617013a6723ad44ee69700a6ad5c7
Charles <zcz958@gmail.com>
2014-09-22 13:20:18 -0700

    first try

--- src/main/java/com/fire/Server.java
+++ src/main/java/com/fire/Server.java
@@ -1,9 +1,25 @@
 package com.fire;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
     public static void main(String[] args)
     {
+      try {
+        ServerSocket sSocket = new ServerSocket(9898);
+        while (true) {
+          Socket socket = sSocket.accept();
+          try {
+            System.out.print(socket.getInputStream());
+          } catch (Exception e) {
 
+          }
+        }
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
     }
 }
---
+++ src/test/java/server/ServerTest.java
@@ -0,0 +1,7 @@
+package java.server;
+
+/**
+ * Created by charles.zhang on 9/22/14.
+ */
+public class ServerTest {
+}
